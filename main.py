d29c5f6ecdf656687a47ef4baf9904d2c9d39cf4
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-03 18:03:10 +0900

    [ UPDATE ] 로그인 선택 기능 삭제

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
+import getpass
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 driver=webdriver.Chrome(ChromeDriverManager().install())
-
-def login_mode(mode):
-    if mode == 'saved':
-        login = {
-            "id" : "id",
-            "pw" : "pw"
-        }
-    elif mode == 'input':
-        pass
+User_id = str(input("아이디를 입력하세요 : "))
+User_pw = getpass.getpass("비밀번호를 입력하세요 : ")
 
 try:
-    driver.get('https://store.leagueoflegends.co.kr/loot') #크롤링 하고싶은 사이트 입력
-    elem=driver.find_element_by_id('container')  #id얻는법 본문에서 설명
-    lis=elem.find_elements_by_xpath('./div[5]/div[2]/div[2]/div[1]/h3') #xpath얻는법 본문에서 설명
+    driver.get('https://store.leagueoflegends.co.kr/loot')
+    # driver.find_element_by_name('id').send_keys('아이디') # "아이디라는 값을 보내준다"
+    driver.find_element_by_name('username').send_keys(User_id)
+    driver.find_element_by_name('password').send_keys(User_pw)
+    print(User_id, User_pw)
+    driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div/button').click()
+    driver.get(driver.current_url)
+    lis=driver.find_elements_by_xpath('//*[@id="lootMaterial"]/ul/li[8]/div/div/span/span/em') #xpath얻는법 본문에서 설명
     for li in lis:
         print(li.text)  
     input()
