85fc2308b85b37f5d24fba1ea9ef2d120a1e3a21
RYUSS - ABCD <ryuryu10@naver.com>
2021-10-06 22:20:36 +0900

    build1

--- main.py
+++ main.py
@@ -1,34 +1,33 @@
+from functools import total_ordering
 import getpass
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+import os.path
+import csv
+
+from webdriver_manager.utils import validate_response
 
 def now_time():
     now = time.localtime()
     return "%04d/%02d/%02d %02d:%02d:%02d " % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
 
-print("[{0}] LOL Token Logger".format(now_time()))
-print("[{0}] 로그인 정보를 입력해주세요.".format(now_time()))
+def now_date():
+    now = time.localtime()
+    return "%04d/%02d/%02d" % (now.tm_year, now.tm_mon, now.tm_mday)
+
 User_id = str(input("아이디를 입력하세요 : "))
 User_pw = getpass.getpass("비밀번호를 입력하세요 : ")
-print("[{0}] 프로세스 시작중...".format(now_time()))
 options = webdriver.ChromeOptions()
-options.add_argument("headless")
 driver=webdriver.Chrome(ChromeDriverManager().install(), options=options)
-print("[{0}] 사이트 접속중...".format(now_time()))
-driver.get('https://store.leagueoflegends.co.kr/loot')
 driver.implicitly_wait(10)
-print("[{0}] 로그인중... 1/3".format(now_time()))
 driver.find_element_by_name('username').send_keys(User_id)
 time.sleep(1)
-print("[{0}] 로그인중... 2/3".format(now_time()))
 driver.find_element_by_name('password').send_keys(User_pw)
 time.sleep(1)
-print("[{0}] 로그인중... 3/3".format(now_time()))
 driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div/button').click()
 time.sleep(3)
-print("[{0}] 계정 정보를 불러오는중... 잠시만 기다려주세요.".format(now_time()))
 driver.get(driver.current_url)
 driver.implicitly_wait(10)
 lis=driver.find_elements_by_xpath('//*[@id="lootMaterial"]/ul/li[8]/div/div/span/span/em')
@@ -36,6 +35,32 @@ for li in lis:
     print("[{0}] {1}의 패스토큰을 확인하였습니다.".format(now_time(), li.text))
 
 print("[{0}] 로깅준비중...".format(now_time()))
+driver.quit()
+
+while True:
+    to_day = now_date()
+    if os.path.isfile('{0}.csv'.format(now_date())):
+        csv_file = open('{0}.csv'.format(now_date()),'a',newline='')
+        csv_write= csv.writer(csv_file)
+        csv_write.writerow(['123','123'])
+        csv_file.close()
+    else:
+        pass
+    if to_day == validate_response:
+        total_ordering
+    else:
+        driver.quit()
+        options = webdriver.ChromeOptions()
+        driver=webdriver.Chrome(ChromeDriverManager().install(), options=options)
+        driver.implicitly_wait(10)
+        driver.find_element_by_name('username').send_keys(User_id)
+        time.sleep(1)
+        driver.find_element_by_name('password').send_keys(User_pw)
+        time.sleep(1)
+        driver.find_element_by_xpath('/html/body/div/div/div/div[2]/div/div/button').click()
+        time.sleep(3)
+        driver.get(driver.current_url)
+
 
 while True:
     print("[{0}] 계정 정보를 새로 불러오는중...".format(now_time()))
@@ -43,12 +68,14 @@ while True:
     driver.implicitly_wait(10)
     lis=driver.find_elements_by_xpath('//*[@id="lootMaterial"]/ul/li[8]/div/div/span/span/em')
     print("[{0}] 정보를 추가하는중...".format(now_time()))
+
     f = open("log.txt",'a')
     for li in lis:
         print("[{0}] {1}의 패스토큰을 확인하였습니다.".format(now_time(), li.text))
         data = now_time() + li.text + '\n'
         f.write(data)
     f.close()
+
     print("[{0}] 대기중... 10분남음".format(now_time()))
     time.sleep(60)
     print("[{0}] 대기중... 9분남음".format(now_time()))
